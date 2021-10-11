41f1a121dd46847cb7249ae97495dc12dac074fb
svrodela <svrodela@gmail.com>
2021-10-11 12:02:46 -0500

    conexion a bd

--- src/app.js
+++ src/app.js
@@ -3,22 +3,27 @@ const log = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
+
 const indexRoutes = require('./routes/index.js');
 
-// escuchar al servidor
-app.set('port',process.env.PORT || 3000);
-//se apuntan a las vistas
-app.set('views',path.join(__dirname,'views'));
-//se configura el motor de vistas
-app.set('view engine','ejs');
+//escuchar servidor
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view egine', 'ejs');
+// conexion a BD
+mongoose.connect('mongodb+srv://sergio_test:sEelYOr4XXdzGtPe@clustersergio.ponuf.mongodb.net/iot?retryWrites=true&w=majority')
+.then(bd => console.log('BD se conectó')).catch(err =>console.log(err));
 
 //middleware
 app.use(log('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-//Rutas
+
+//rutas 
 app.use('/', indexRoutes);
 
-app.listen(app.get('port'),() =>{
-    console.log('Servidor en el puerto ', app.get('port'))
+
+app.listen(app.get('port'), () =>{
+    console.log('servidor funcionando en el puerto', app.get('port'))
 });
